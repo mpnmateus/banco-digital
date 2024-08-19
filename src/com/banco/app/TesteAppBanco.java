95b1441c92d04340c80d6e37298e959f6cb6fd9e
mateusnovo <mpnmateus@gmail.com>
2024-08-19 15:55:53 -0300

    feat: executando testes de instancacao: cliente e tipos de banco

--- src/com/banco/app/TesteAppBanco.java
+++ src/com/banco/app/TesteAppBanco.java
@@ -1,4 +1,14 @@
 package com.banco.app;
 
+import com.banco.model.Cliente;
+import com.banco.model.Conta;
+import com.banco.model.ContaCorrente;
+
 public class TesteAppBanco {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente("Mateus");
+
+        Conta c1 = new ContaCorrente(cliente);
+    }
 }
