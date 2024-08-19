d790addc4c2e5295dfd7d99a95a7abf82cc0a41a
mateusnovo <mpnmateus@gmail.com>
2024-08-19 15:55:00 -0300

    feat: implementando metodos da sua interface

--- src/com/banco/model/Conta.java
+++ src/com/banco/model/Conta.java
@@ -1,12 +1,29 @@
 package com.banco.model;
 
-public class Conta {
+import com.banco.service.IConta;
+
+public class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
     protected int agencia;
     protected int numero;
     protected Cliente cliente;
 
+
     public Conta (Cliente cliente){
         this.cliente = cliente;
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
     }
+
+    public void sacar(double valor){};
+
+    public void depositar(double valor){};
+
+    public void transferir(double valor, Conta contaDestino){};
+
+    public void imprimirExtrato(){};
 
 }
