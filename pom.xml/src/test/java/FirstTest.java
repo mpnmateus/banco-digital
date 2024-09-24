c51b70c1d48e8aa7647aab2a406288783e951fcb
mateusnovo <mpnmateus@gmail.com>
2024-09-24 16:34:31 -0300

    Adicionado configuracao maven com dependencia do JUnit. Alterada a estrutura de diretorios

---
+++ pom.xml/src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void testSoma() {
+        assertEquals(5, 2 + 3);
+    }
+
+}
