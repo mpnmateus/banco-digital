e96d3685a251ba42c5691db84c672bffdeeee9fd
mateusnovo <mpnmateus@gmail.com>
2024-09-26 14:19:48 -0300

    feat: testando transferencia (metodo Conta) entre contas correntes - OK

--- src/test/java/ContaTeste.java
+++ src/test/java/ContaTeste.java
@@ -27,5 +27,17 @@ public class ContaTeste {
         Assertions.assertEquals(1500, conta.getSaldo(), "O saldo após o depósito deve ser R$ 1500,00");
     }
 
+    @Test
+    public void efetuarTransferenciaTeste(){
+        Conta contaDestino = new ContaCorrente(new Cliente("Mateus"));
+        contaDestino.depositar(500);
+
+        conta.transferir(500, contaDestino);
+
+        Assertions.assertEquals(500, conta.getSaldo(), "O saldo da conta original deve ser " +
+                "R$ 500,00 após a transferência, pois tinha saldo R$ 1000,00");
+        Assertions.assertEquals(1000, contaDestino.getSaldo(), "O saldo da conta de " +
+                "destino deve ser R$ 1000,00 após a transferência, pois iniciou com saldo R$ 500,00");
+    }
 }
 
